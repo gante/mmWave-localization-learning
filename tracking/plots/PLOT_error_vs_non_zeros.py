a7fad58fcf10bf268fa718f5329a131e20fc4491
João Gante <joaofranciscocardosogante@gmail.com>
2019-02-23 14:58:38 +0000

    Minor fixes

--- tracking/plots/PLOT_error_vs_non_zeros.py
+++ tracking/plots/PLOT_error_vs_non_zeros.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 
 
 MEMORY = 7 #there are at least these non-zeros per sample
+START_PLOT = 6
+END_PLOT = 2500
 
 
 #------------------------------------------------------------------------------
@@ -97,7 +99,7 @@ for i in range(n_bins):
         non_zero_bins_bot[i] = non_zero_bins_bot[i-1]
         non_zero_bins_top[i] = non_zero_bins_top[i-1]
 
-x = list(range(n_bins))
+x = list(range(1, n_bins+1))
 bin_counts = np.asarray(bin_counts)
 assert len(x) == len(bin_counts) == len(non_zero_bins_mean), "{} vs {} vs {}".\
     format(len(x), len(bin_counts), len(non_zero_bins_mean))
@@ -114,9 +116,9 @@ for i in range(cropped_entries):
     non_zero_bins_mean[i] = non_zero_bins_mean[cropped_entries]
 
 ax1 = plt.subplot(211)
-ax1.plot(x,bin_counts, color='tab:orange')
-ax1.set_xlim([0,n_bins])
-ax1.set_ylim([0,np.max(bin_counts)])
+ax1.semilogx(x[START_PLOT:END_PLOT],bin_counts[START_PLOT:END_PLOT], color='tab:orange')
+ax1.set_xlim([START_PLOT+1,END_PLOT+1])
+ax1.set_ylim([0,np.max(bin_counts[START_PLOT:END_PLOT])])
 # ax1.set_ylim([0,4000])
 ax1.set_ylabel("Sequence Count")
 # ax1.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
@@ -124,14 +126,16 @@ ax1.grid(True)
 
 
 plt.subplot(212)
-plt.plot(x, non_zero_bins_mean, label='Average Error')
-plt.fill_between(x, non_zero_bins_bot, non_zero_bins_top, facecolor='tab:blue', alpha=0.3, label = '$95\%$ Interval')
+plt.semilogx(x[START_PLOT:END_PLOT], non_zero_bins_mean[START_PLOT:END_PLOT], label='Average Error')
+plt.fill_between(x[START_PLOT:END_PLOT], non_zero_bins_bot[START_PLOT:END_PLOT],
+    non_zero_bins_top[START_PLOT:END_PLOT], facecolor='tab:blue', alpha=0.3,
+    label = '$95\%$ Interval')
 plt.legend()
-plt.xlim([0,n_bins])
+plt.xlim([START_PLOT+1,END_PLOT+1])
 # plt.ylim([0,min(np.max(non_zero_bins_top), 50)])
-plt.ylim([0,min(np.max(non_zero_bins_top), 20)])
+plt.ylim([0,min(np.max(non_zero_bins_top[START_PLOT:END_PLOT]), 20)])
 plt.ylabel("Error (m)")
-plt.xlabel("Number of Detected (Non-zero) Paths")
+plt.xlabel("Number of Detected Paths (log scale)")
 plt.grid(True)
 
 plt.savefig('error_vs_nonzero.pdf', format='pdf')
