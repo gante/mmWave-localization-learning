036e394aa57fe91975c4111e2f6012272c253927
aminmalekisadr <74942849+aminmalekisadr@users.noreply.github.com>
2021-03-09 16:36:08 -0500

    New changes

--- bff_positioning/visualization/plot_uncertainty.py
+++ bff_positioning/visualization/plot_uncertainty.py
@@ -15,9 +15,14 @@ import numpy as np
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+import numpy as np
 
-N_SAMPLES = 20
+N_SAMPLES =8
 GRID_SIZE = 400
 PLOT_PATH = "uncertainty.jpg"
 
@@ -29,20 +34,46 @@ def plot_uncertainty(y_true, y_pred, background_data):
     :param y_pred: model samples
     :param background_data: numpy array containing the positions with data
     """
+    fig, ax = plt.subplots()
     # Flips y (imshow assumes 0 on top, growing down)
     y_true[:, 1] = -y_true[:, 1] + GRID_SIZE
     y_pred[:, 1] = -y_pred[:, 1] + GRID_SIZE
 
     # Plots the background in black and black (white = positions with data)
-    plt.imshow(-background_data, cmap='Greys', vmin=-1.0, vmax=0.0, alpha=0.5)
+    ax.imshow(-background_data, cmap='Greys', vmin=-1.0, vmax=0.0, alpha=0.8)
 
     # Plots the MC Dropout samples in blue, with some transparency
-    plt.scatter(x=y_pred[:, 0, :], y=y_pred[:, 1, :], c='b', s=2, alpha=0.01)
+    ax.scatter(x=y_pred[:, 0, :], y=y_pred[:, 1, :], c='b', s=4, alpha=0.1)
 
     # Plots the true position in solid red
-    plt.scatter(x=y_true[:, 0], y=y_true[:, 1], c='r', s=5)
+    ax.scatter(x=y_true[:, 0], y=y_true[:, 1], c='r', s=5)
 
-    # Saves the plot
+
+
+   # plt.plot(y_true[:, 0], y_true[:, 1])
+
+
+#    ax.plot(y_true[:, 0], y_true[:, 1])
+
+    axins = zoomed_inset_axes(ax, 2, loc=3)  # zoom = 6
+    for i in  range(y_pred[:, 0, :].shape[1]):
+     axins.scatter(y_pred[0, 0, :], y_pred[0, 1, :],c='b', s=4, alpha=0.1)
+    axins.scatter(y_true[0, 0], y_true[0, 1],color="red")
+
+    axins.set_xlim(y_true[0, 0]-20,y_true[0, 0]+20)  # Limit the region for zoom
+    axins.set_ylim(y_true[0, 1]-20, y_true[0, 1]+20)
+
+    plt.xticks(visible=False)  # Not present ticks
+    plt.yticks(visible=False)
+    #
+    ## draw a bbox of the region of the inset axes in the parent axes and
+    ## connecting lines between the bbox and the inset axes area
+    mark_inset(ax, axins, loc1=4, loc2=4, fc="none", ec="0.5")
+
+    plt.draw()
+    plt.show()
+
+
     plt.savefig(PLOT_PATH, dpi=300)
     logging.info("Plot written to %s", PLOT_PATH)
 
@@ -57,6 +88,7 @@ def main():
         "E.g. `python run_non_tracking_experiment.py <path to .yaml file>`"
     with open(sys.argv[1], "r") as yaml_config_file:
         logging.info("Loading simulation settings from %s", sys.argv[1])
+        #pdb.set_trace()
         experiment_config = yaml.load(yaml_config_file)
     data_parameters = experiment_config['data_parameters']
     experiment_settings = experiment_config['experiment_settings']
